b93c419bb5fe3cbd0924d26e1dcd83ed1404d086
Ahmet Kun <ahmetkun@gmail.com>
2019-12-08 02:29:51 +0300

    add solution for #40

---
+++ 040/40.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	str := ""
+	i := 0
+	for len(str) < 1000000 {
+        i++
+		str = str + strconv.Itoa(i)
+	}
+
+    res := 1
+    for _, i := range []int{0, 9, 99, 999, 9999, 99999, 999999} {
+        c, _ := strconv.Atoi(fmt.Sprintf("%c", str[i]))
+        res *= c
+    }
+    
+    fmt.Println(res)
+}
