bce7274ea735c70cc63870c98a9efe60da710cab
Ahmet Kun <ahmetkun@gmail.com>
2019-12-08 02:32:45 +0300

    fix gofmt

--- 040/40.go
+++ 040/40.go
@@ -9,15 +9,15 @@ func main() {
 	str := ""
 	i := 0
 	for len(str) < 1000000 {
-        i++
+		i++
 		str = str + strconv.Itoa(i)
 	}
 
-    res := 1
-    for _, i := range []int{0, 9, 99, 999, 9999, 99999, 999999} {
-        c, _ := strconv.Atoi(fmt.Sprintf("%c", str[i]))
-        res *= c
-    }
-    
-    fmt.Println(res)
+	res := 1
+	for _, i := range []int{0, 9, 99, 999, 9999, 99999, 999999} {
+		c, _ := strconv.Atoi(fmt.Sprintf("%c", str[i]))
+		res *= c
+	}
+
+	fmt.Println(res)
 }
